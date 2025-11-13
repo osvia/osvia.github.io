4f7a341ceca3981da60e132bc9c04eafcb272ff3
ocram444 <s2022544@uni-wuppertal.de>
2025-11-13 23:40:23 +0100

    added wip content management system

--- _site/assets/assets/js/lunr/lunr-store.js
+++ _site/assets/assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
+        "title": "ICCV 2025 Paper Acceptance",
+        "excerpt":"Body (optional).  ","categories": ["ICCV2025","Paper"],
+        "tags": [],
+        "url": "/Team-Website/news/project4/",
+        "teaser": null
+      },{
+        "title": "Test",
+        "excerpt":"Body (optional).  ","categories": ["ICCV2025","Paper"],
+        "tags": [],
+        "url": "/Team-Website/news/test/",
+        "teaser": null
+      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/Shape-Bias-and-Robustness-Evaluation-via-Cue-Decomposition-for-Image-Classification-and-Segmentation/jekyll/update/2025/05/02/welcome-to-jekyll.html",
+        "url": "/Team-Website/jekyll/update/2025/05/02/welcome-to-jekyll.html",
         "teaser": null
       }]
